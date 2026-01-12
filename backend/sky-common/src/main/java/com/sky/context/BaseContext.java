27097042f580ddc322d391ba7f954a41ee69e24f
MangoMilk666 <2767620910@qq.com>
2026-01-13 00:32:52 +0800

    fix: use ThreadLocal to get current user_id for update_user column

--- backend/sky-common/src/main/java/com/sky/context/BaseContext.java
+++ backend/sky-common/src/main/java/com/sky/context/BaseContext.java
@@ -1,5 +1,9 @@
 package com.sky.context;
 
+/**
+ * 上下文工具类
+ * 通过Thread的局部变量ThreadLocal,在同一个线程内传递当前用户的ID值
+ */
 public class BaseContext {
 
     public static ThreadLocal<Long> threadLocal = new ThreadLocal<>();
